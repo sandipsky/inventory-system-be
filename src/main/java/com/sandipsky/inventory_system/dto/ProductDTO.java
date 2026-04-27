c264476a6815a2dee4dcb317452d5cedaf231d56
sandip.shakya@maitriservices.com <sandip.shakya@maitriservices.com>
2026-04-27 17:45:18 +0545

    changes

--- src/main/java/com/sandipsky/inventory_system/dto/ProductDTO.java
+++ src/main/java/com/sandipsky/inventory_system/dto/ProductDTO.java
@@ -1,5 +1,8 @@
 package com.sandipsky.inventory_system.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -31,4 +34,15 @@ public class ProductDTO {
     private boolean isPurchasable;
     private boolean isSellable;
     private boolean isServiceItem;
+
+    public List<String> getProductTypes() {
+        List<String> types = new ArrayList<>();
+        if (isPurchasable) {
+            types.add("Purchasable");
+        }
+        if (isSellable) {
+            types.add("Sellable");
+        }
+        return types;
+    }
 }
